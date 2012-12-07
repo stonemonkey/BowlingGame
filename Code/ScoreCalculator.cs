d67b780709d74d5c9b4a49f78d26b16f21e7bdc5
stonemonkey <stonemonkey77ro@yahoo.com>
2012-12-07 17:08:49 +0200

    first revision.

---
+++ Code/ScoreCalculator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class ScoreCalculator
+    {
+        private const int RolsPerFrame = 2;
+        private const int MaxNumberOfRols = 21;
+        private const int MaxNumberOfFrames = 10;
+        private const int MaxNumberOfPinsKnockedDownPerFrame = 10;
+        private const int FirstRollOfLastFrame = 18;
+
+        private int _currentRoll;
+        private int _firstRollOfCurrentFrame;
+        private readonly int[] _pins = new int[MaxNumberOfRols];
+
+        public int GetScore()
+        {
+            var score = 0;
+
+            for (var frame = 0; frame < MaxNumberOfFrames; frame++)
+                score += GetFrameScore(frame);
+
+            return score;
+        }
+
+        private int GetFrameScore(int frame)
+        {
+            _firstRollOfCurrentFrame = (frame * RolsPerFrame);
+
+            var score = GetFramePins();
+
+            if (IsStrike()) 
+                score += GetStrikeBonus();
+            else if (IsSpare()) 
+                score += GetSpareBonus();
+
+            return score;
+        }
+
+        private int GetSpareBonus()
+        {
+            return _pins[_firstRollOfCurrentFrame + 2];
+        }
+
+        private bool IsSpare()
+        {
+            return AreAllKnockedDown(GetFramePins());
+        }
+
+        private int GetFramePins()
+        {
+            return (_pins[_firstRollOfCurrentFrame] + _pins[_firstRollOfCurrentFrame + 1]);
+        }
+
+        private int GetStrikeBonus()
+        {
+            var bonus = _pins[_firstRollOfCurrentFrame + 2];
+
+            if (IsNotInLastFrame(_firstRollOfCurrentFrame))
+                bonus += GetNormalFrameStrikeBonus();
+
+            return bonus;
+        }
+
+        private int GetNormalFrameStrikeBonus()
+        {
+            var bonus = _pins[_firstRollOfCurrentFrame + 3];
+
+            if (bonus == 0)
+                bonus += _pins[_firstRollOfCurrentFrame + 4];
+            
+            return bonus;
+        }
+
+        private bool IsStrike()
+        {
+            return AreAllKnockedDown(GetFirstRollPins());
+        }
+
+        private int GetFirstRollPins()
+        {
+            return _pins[_firstRollOfCurrentFrame];
+        }
+
+        public void Roll(int pins)
+        {
+            if ((pins < 0) ||
+                (pins > MaxNumberOfPinsKnockedDownPerFrame))
+                throw new ArgumentOutOfRangeException();
+
+            // TODO: I do not like next code here. I presume it can be refactored ... using a list instead of an array
+            _pins[_currentRoll++] = pins;
+
+            if (AreAllKnockedDown(pins) && IsNotInLastFrame(_currentRoll))
+                _currentRoll++;
+        }
+
+        private static bool IsNotInLastFrame(int roll)
+        {
+            return (roll < FirstRollOfLastFrame);
+        }
+
+        private static bool AreAllKnockedDown(int pins)
+        {
+            return (pins == MaxNumberOfPinsKnockedDownPerFrame);
+        }
+    }
+}
---
+++ Code/ScoreCalculatorTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BowlingGame
+{
+    [TestFixture]
+    public class ScoreCalculatorTests
+    {
+        private ScoreCalculator _scoreCalculator;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            _scoreCalculator = new ScoreCalculator();
+        }
+
+        [Test]
+        public void Can_create_game()
+        {
+            Assert.IsNotNull(_scoreCalculator);
+        }
+
+        [Test]
+        public void GetScore_returns_0_without_rolling()
+        {
+            var score = _scoreCalculator.GetScore();
+
+            Assert.AreEqual(0, score);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(10, 1)]
+        [TestCase(1, 9)]
+        [TestCase(2, 20)]
+        public void GetScore_returns_sum_of_knocked_down_pins_when_rolling_many_times(int knockedDownPins, int numberOfRolls)
+        {
+            RollMany(numberOfRolls, knockedDownPins);
+
+            var score = _scoreCalculator.GetScore();
+
+            Assert.AreEqual((knockedDownPins * numberOfRolls), score);
+        }
+
+        [Test]
+        public void GetScore_returns_game_score_having_one_spare_frame()
+        {
+            _scoreCalculator.Roll(5);
+            _scoreCalculator.Roll(5);
+            RollMany(18, 1);
+
+            var score = _scoreCalculator.GetScore();
+
+            Assert.AreEqual(29, score);
+        }
+
+        [Test]
+        public void GetScore_returns_150_rolling_spare_in_all_frames()
+        {
+            RollMany(21, 5);
+
+            var score = _scoreCalculator.GetScore();
+
+            Assert.AreEqual(150, score);
+        }
+
+        [Test]
+        public void GetScore_returns_game_score_having_one_strike_frame()
+        {
+            _scoreCalculator.Roll(10);
+            RollMany(18, 1);
+
+            var score = _scoreCalculator.GetScore();
+
+            Assert.AreEqual(30, score);
+        }
+
+        [Test]
+        public void GetScore_returns_300_rolling_strike_in_all_frames()
+        {
+            RollMany(12, 10);
+
+            var score = _scoreCalculator.GetScore();
+
+            Assert.AreEqual(300, score);
+        }
+
+        [TestCase(-1)]
+        [TestCase(-99)]
+        [TestCase(11)]
+        [TestCase(102)]
+        public void Roll_throws_when_pins_are_out_of_range(int pins)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _scoreCalculator.Roll(pins));
+        }
+
+        private void RollMany(int numberOfRolls, int numberOfKnockedDownPins)
+        {
+            for (var i = 0; i < numberOfRolls; i++)
+                _scoreCalculator.Roll(numberOfKnockedDownPins);
+        }
+    }
+}
